94ce0426bd84607bd9ec8c4c25630d60d541098e
Petr Martynov <marsyn01@gmail.com>
2026-04-10 13:50:10 +0500

    Add /about-us route to server

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const PORT = process.env.PORT || 3000;
 // Read HTML files at startup
 const htmlPath = path.join(__dirname, 'index.html');
 const schoolsPath = path.join(__dirname, 'for-schools.html');
+const aboutUsPath = path.join(__dirname, 'about-us.html');
 
-let html, schoolsHtml;
+let html, schoolsHtml, aboutUsHtml;
 
 try {
   html = fs.readFileSync(htmlPath, 'utf8');
@@ -25,22 +26,31 @@ try {
   console.error('Failed to load for-schools.html:', e.message);
 }
 
+try {
+  aboutUsHtml = fs.readFileSync(aboutUsPath, 'utf8');
+  console.log('Loaded about-us.html (' + aboutUsHtml.length + ' bytes)');
+} catch(e) {
+  aboutUsHtml = '<h1>Error: Could not load about-us.html</h1><p>' + e.message + '</p>';
+  console.error('Failed to load about-us.html:', e.message);
+}
+
 http.createServer((req, res) => {
   const url = req.url.split('?')[0].replace(/\/$/, '') || '/';
 
+  let content;
   if (url === '/for-schools') {
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8',
-      'Content-Length': Buffer.byteLength(schoolsHtml)
-    });
-    res.end(schoolsHtml);
+    content = schoolsHtml;
+  } else if (url === '/about-us') {
+    content = aboutUsHtml;
   } else {
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8',
-      'Content-Length': Buffer.byteLength(html)
-    });
-    res.end(html);
+    content = html;
   }
+
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8',
+    'Content-Length': Buffer.byteLength(content)
+  });
+  res.end(content);
 }).listen(PORT, '0.0.0.0', () => {
   console.log('Scholastic Chess HK running on 0.0.0.0:' + PORT);
 });
